554e7df2f8d66d0a0088c8b238fce1cc9b2215e3
winslettemicah <winslettemicah@gmail.com>
2018-09-29 19:00:01 +0800

    Initial commit

---
+++ app/src/main/java/iics/sanidad/winslettemicah/lab3_sanidad/MainActivity.java
@@ -0,0 +1,31 @@
+package iics.sanidad.winslettemicah.lab3_sanidad;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+/*
+        Button NextButt =(Button) findViewById(R.id.button4);
+        Button BackButt =(Button) findViewById(R.id.button3);
+        NextButt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+        BackButt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+*/
+    }
+}
